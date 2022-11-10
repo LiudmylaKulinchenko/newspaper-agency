d6e4fe82fdd8e944896fa3769fd7d43fdc7a8974
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-11-10 12:44:06 +0100

    Add to model Topic class Meta with ordering & reassign str method

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Topic (models.Model):
+class Topic(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
