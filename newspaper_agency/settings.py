27203a4295d057dab0d31b5c1813432274428a53
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-12-07 00:00:33 +0200

    Fix settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -23,7 +23,7 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, 'static'),
+    os.path.join(CORE_DIR, "static"),
 )
 
 STATIC_ROOT = "staticfiles/"
